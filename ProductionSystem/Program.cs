09413d84184e9773f209682914482ad7e8e8c2a6
Rostick <Rostick@KOMPUTER>
2026-04-28 23:53:01 +0300

    Fix Program.cs for .NET 8 compatibility, add Swagger

--- ProductionSystem/Program.cs
+++ ProductionSystem/Program.cs
@@ -15,19 +15,18 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.InstanceName = "ProductionSystem_";
 });
 
-// Add services to the container.
-
+// Добавляем контроллеры
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Swagger для тестирования API
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Включаем Swagger
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
